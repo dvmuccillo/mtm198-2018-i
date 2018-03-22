0e79c5683420a39b577383cb0f8d6c9908ad0d7d
evandro <evandrolrff@gmail.com>
2018-03-21 22:33:05 -0300

    Copiado conteudo do site mandado no link, com alguns comentários.

--- geraGraficos.py
+++ geraGraficos.py
@@ -1,33 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 
-def sequencia_limitada(self):
-    pass
-
-def sequencia_monotona(self):
-    pass
-
-def sequencia_convergente(self):
-    pass
-
-
-if __name__ == "__main__":
+def sequencia(formula, max, label_sequencia):
     sequencia = []
-    data1 = [10, 5, 2, 4, 6, 8]
-    data2 = [1, 2, 4, 8, 7, 4]
-    x = 10 * np.array(range(len(data1)))
+    for i in range(1,max):
+        sequencia.append(math.pow(formula,i))
 
-    plt.plot(x, data1, 'go')  # green bolinha
-#    plt.plot(x, data1, 'k:', color='orange')  # linha pontilha orange
+    x = 10 * np.array(range(len(sequencia)))
 
-    plt.plot(x, data2, 'r^')  # red triangulo
-#    plt.plot(x, data2, 'k--', color='blue')  # linha tracejada azul
+    plt.plot(x, sequencia, 'go', label=label_sequencia)  # green bolinha
+    #   plt.plot(x, data1, 'k:', color='orange')  # linha pontilha orange
 
-#    plt.axis([-10, 60, 0, 11])    dois primeiros valores, intervalo eixo X, e os dois ultimos, intervalo eixo Y.
+    ''' dois primeiros valores, intervalo eixo X, e os dois ultimos, intervalo eixo Y
+        plt.axis([-10, 60, 0, 11])'''
     plt.title("Gráficos Sequências")
 
+    plt.subplot().legend(loc='best', shadow=False, fontsize='large')
     plt.grid(True)
     plt.xlabel("n E N")
     plt.ylabel("R")
     plt.show()
+
+
+if __name__ == "__main__":
+
+    sequencia((1/2), 10, 'Sequencia convergente')
+
+    sequencia((-2), 8, 'Sequencia divergente')
